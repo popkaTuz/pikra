eca29606b56262473b32cd3263ee301544a85611
Sundeep <97897333+popkaTuz@users.noreply.github.com>
2023-01-18 18:18:45 +0200

    Create kiri.py

---
+++ kiri.py
@@ -0,0 +1,85 @@
+def func1(arg1, arg2):
+    if arg1 < arg1:
+        var7 = class2()
+    else:
+        var7 = class4()
+    for var8 in [(-4 - i) ^ -9 for i in range(34)]:
+        var9 = var7.func3
+        var9(arg1, arg1)
+    def func6(arg10, arg11):
+        var12 = (arg10 - arg11) | arg2 & 51502427
+        var13 = arg11 & arg11
+        var14 = arg1 - var13 | arg2
+        var15 = var13 & arg1 | var12 + arg10
+        var16 = (365 - arg2 | arg2) + 481
+        var17 = 975561117 + var16 | var15 & var16
+        var18 = (var13 - -764 | var12) - var12
+        var19 = var12 | var14
+        if arg2 < var19:
+            var20 = arg2 + (arg1 | var16 & arg11)
+        else:
+            var20 = var17 ^ (arg1 - arg11) + var15
+        var21 = var18 ^ arg11
+        var22 = arg10 + arg2
+        if arg10 < var13:
+            var23 = ((arg1 - var17) & var22) | 1048379542
+        else:
+            var23 = (arg10 + var16) | var22
+        var24 = var15 & var13
+        var25 = -132280189 | var17
+        var26 = var14 - var22
+        var27 = var26 | arg11 - (var14 | var18)
+        var28 = (var21 + var21 ^ var12) ^ var12
+        var29 = (var22 | (var27 + var28)) ^ var19
+        var30 = var27 - var22 + (var15 | var21)
+        var31 = var26 + var21
+        var32 = arg10 + arg11
+        if var25 < var32:
+            var33 = (arg10 | var28) + var25 & arg2
+        else:
+            var33 = var18 + (var19 | var18 & var12)
+        var34 = var24 - var26 - var22 & var32
+        result = var17 - var15
+        return result
+    var35 = func6(arg2, arg1)
+    var36 = func9()
+    var37 = func12()
+    var38 = (var37 | (-439 + (arg1 - (((arg1 - var36) | -870) ^ arg2) & 589 & (-704 + var37) - -259 + (var36 + var35) - -1815087470 + var35 + (var35 & arg1 - var36) ^ 851) - arg2) + 74) + -501
+    result = arg1 - var38
+    return result
+def func12():
+    func10()
+    result = len(range(1))
+    func11()
+    return result
+def func11():
+    global len
+    del len
+def func10():
+    global len
+    len = lambda x : 7
+def func9():
+    func7()
+    result = len(range(39))
+    func8()
+    return result
+def func8():
+    global len
+    del len
+def func7():
+    global len
+    len = lambda x : 9
+class class4(object):
+    def func3(self, arg5, arg6):
+        return 0
+class class2(class4):
+    def func3(self, arg3, arg4):
+        return 0
+if __name__ == "__main__":
+    print 'prog_size: 5'
+    print 'func_number: 13'
+    print 'arg_number: 39'
+    for i in xrange(25000):
+        x = 5
+        x = func1(x, i)
+        print x,
